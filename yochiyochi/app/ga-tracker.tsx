05fff4960c5f84b0613652395fc2bef1d0b23e0c
YUYAcanine <futonyuhoninsan@gmail.com>
2025-09-13 14:10:19 +0900

    ga-tracker

--- yochiyochi/app/ga-tracker.tsx
+++ yochiyochi/app/ga-tracker.tsx
@@ -1,9 +1,9 @@
 "use client";
 
 import { usePathname, useSearchParams } from "next/navigation";
-import { useEffect } from "react";
+import { useEffect, Suspense } from "react";
 
-export default function GaTracker() {
+function GaInnerTracker() {
   const pathname = usePathname();
   const searchParams = useSearchParams();
   const GA_ID = process.env.NEXT_PUBLIC_GA_ID;
@@ -11,9 +11,17 @@ export default function GaTracker() {
   useEffect(() => {
     if (!GA_ID) return;
     const url = pathname + searchParams.toString();
-    // @ts-expect-error gtag is provided by Google Analytics script
+    // @ts-expect-error gtag is provided by GA script
     window.gtag("config", GA_ID, { page_path: url });
   }, [pathname, searchParams, GA_ID]);
 
   return null;
+}
+
+export default function GaTracker() {
+  return (
+    <Suspense fallback={null}>
+      <GaInnerTracker />
+    </Suspense>
+  );
 }
