9ab013bb4095d80785b3eb41a8480d3559c9ba37
YUYAcanine <futonyuhoninsan@gmail.com>
2025-09-14 00:10:20 +0900

    add GA event

---
+++ yochiyochi/lib/gtag.ts
@@ -0,0 +1,27 @@
+// lib/gtag.ts
+export const GA_ID = process.env.NEXT_PUBLIC_GA_ID || "";
+
+export const pageview = (url: string) => {
+  if (typeof window !== "undefined" && GA_ID) {
+    window.gtag("config", GA_ID, {
+      page_path: url,
+    });
+  }
+};
+
+type GtagEvent = {
+  action: string;
+  category?: string;
+  label?: string;
+  value?: number;
+};
+
+export const event = ({ action, category, label, value }: GtagEvent) => {
+  if (typeof window !== "undefined" && GA_ID) {
+    window.gtag("event", action, {
+      event_category: category,
+      event_label: label,
+      value: value,
+    });
+  }
+};
